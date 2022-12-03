ba07141c962810a02a5093f59061346cdfc9d7b7
kokuyouwind <kokuyouwind@gmail.com>
2022-12-03 23:13:38 +0900

    client/server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Client } from 'boardgame.io/react';
 import { TicTacToe } from './Game';
 import { TicTacToeBoard } from "./Board";
+import { SocketIO } from 'boardgame.io/multiplayer'
 
-const App = Client({
+const TicTacToeClient = Client({
   game: TicTacToe,
   board: TicTacToeBoard,
+  multiplayer: SocketIO({ server: 'localhost:8000' }),
 });
 
-export default App;
+export default TicTacToeClient;
